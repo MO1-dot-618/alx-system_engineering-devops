10a3f9baa489f67c1235221f74f910930cbc7fb8
Malika Oubilla <malikaoubilla@gmail.com>
2023-11-28 10:09:43 -0800

    100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/from:\K\S+\b/).join
-echo ","
-puts ARGV[0].scan(/to:\K\S+\b/).join
-echo ","
-puts ARGV[0].scan(/flags:\K\S+\b/).join
+puts ARGV[0].scan(/from:\K\S+\b|to:\K\S+\b|flags:\K\S+\b/).join(",")
